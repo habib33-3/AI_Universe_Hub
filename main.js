656049bde5e960963702080cd5cfe5b4ffa301fa
Habibur Rahman <habib.swi16@gmail.com>
2023-08-30 01:00:51 +0600

    card added

--- main.js
+++ main.js
@@ -1,9 +1,55 @@
 const loadTool = async () => {
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/ai/tool/01"
-  );
+  const res = await fetch("https://openapi.programming-hero.com/api/ai/tools");
   const data = await res.json();
-  console.log(data.data);
+
+  const tools = data.data.tools;
+
+  console.log(typeof tools);
+  console.log(tools[1].name);
+  displayTools(tools);
+};
+
+const displayTools = (tools) => {
+  const toolContainer = document.getElementById("tool-container");
+
+  tools.forEach((tool) => {
+    const toolCard = document.createElement("div");
+
+    toolCard.classList = `w-96 rounded-lg card shadow-lg border-solid bg-gray-200 `;
+    toolCard.innerHTML = `
+    <div  class="p-8">
+    <img src="${tool.image}" alt="" />
+    <div>
+      <h4 class="text-2xl text-black font-semibold">Features</h4>
+      <ol
+        class="list-decimal text-slate-800 my-5 border-b border-solid border-gray-600"
+      >
+        <li>${tool.features[0]}</li>
+        <li>${tool.features[1]}</li>
+        <li>${tool.features[2]}</li>
+      </ol>
+    </div>
+
+    <div class="flex justify-between">
+      <div>
+        <h2 class="text-2xl text-black font-semibold">${tool.name}</h2>
+    
+        <p class="text-gray-500 font-medium mt-2">${tool.published_in}</p>
+      </div>
+
+      <div>
+        <a
+          id="show-details"
+          class="text-2xl tex-black font-semibold"
+          href=""
+          ><i class="fa-solid fa-arrow-right"></i
+        ></a>
+      </div>
+    </div>
+  </div>
+`;
+    toolContainer.appendChild(toolCard);
+  });
 };
 
 loadTool();
